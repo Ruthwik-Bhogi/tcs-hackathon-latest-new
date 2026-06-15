e36be52c19eebc2268790d26960cf9340abfce8a
Ruthwik-Bhogi <74715408+Ruthwik-Bhogi@users.noreply.github.com>
2026-06-16 00:34:07 +0530

    Add files via upload

---
+++ HospitalMind_Hackathon_EndToEnd/src/rag/retriever.py
@@ -0,0 +1,7 @@
+
+class SOPRetriever:
+    def retrieve(self,question):
+        return [
+            'If ICU occupancy exceeds 90%, open overflow capacity.',
+            'Allocate additional ventilators during surges.'
+        ]
